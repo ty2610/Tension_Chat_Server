6b062e6493610457cac631bd9fc1e0c1a79721e5
tyler.green <tyler.green@reeher.com>
2018-05-10 19:53:47 -0500

    Fix Hide Show Issue

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-    $('body').hide();
-    $(window).on('load', function() {
-        $('body').show();
-    });
-});
-
 var mainApp = angular.module('mainApp', []).config(['$locationProvider', function($locationProvider) { $locationProvider.html5Mode({ enabled: true, requireBase: false }); }]);
 
 mainApp.controller('MainController', function IndexController($scope, $location, $http, $window) {
